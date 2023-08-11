3fb6a46688c29e3924b5a469539d56197d5bf7a8
Muhammad Shahid <112244414+sheikhmuhammadshahid@users.noreply.github.com>
2023-08-11 17:40:21 +0500

    first coment

---
+++ PCappServer/Form1.cs
@@ -0,0 +1,172 @@
+﻿using PCappServer.classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PCappServer
+{
+    public partial class Form1 : Form
+    {
+        string pressedBy = "-1";
+        string roundType="-1";
+        string questionNo = "-1";
+
+
+        private Socket listenerSocket;
+        private List<User> clientSockets = new List<User>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void Start()
+        {
+            listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenerSocket.Bind(new IPEndPoint(IPAddress.Any, 8888));
+            listenerSocket.Listen(10);
+
+            Console.WriteLine("Server started...");
+
+            Thread listenThread = new Thread(ListenForClients);
+            listenThread.Start();
+        }
+
+        public void Stop()
+        {
+            listenerSocket.Close();
+
+            foreach (User clientSocket in clientSockets)
+            {
+                clientSocket.Socket.Close();
+            }
+        }
+
+        private void ListenForClients()
+        {
+            while (true)
+            {
+                Socket clientSocket = listenerSocket.Accept();
+                Console.WriteLine("Client connected: " + clientSocket.RemoteEndPoint);
+
+              
+
+                Thread clientThread = new Thread(() => HandleClient(clientSocket));
+                clientThread.Start();
+            }
+        }
+
+        private void HandleClient(Socket clientSocket)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = clientSocket.Receive(buffer);
+                string name = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                if (!clientSockets.Any(s => s.Name.ToLower() == name.ToLower()) || name.ToLower() == "admin" || name.ToLower() == "admin1")
+                {
+                    try
+                    {
+                        BroadcastMessageToAll("connected:" + name);
+                        while (true)
+                        {
+                            buffer = new byte[1024];
+                            bytesRead = clientSocket.Receive(buffer);
+
+                            if (bytesRead == 0)
+                            {
+                                Console.WriteLine("Client disconnected: " + name);
+                                BroadcastMessageToAll("disconnected:" + name);
+
+                                clientSockets.Remove(clientSockets.Where(s => s.Socket == clientSocket).SingleOrDefault());
+                                clientSocket.Close();
+
+                                break;
+                            }
+
+                            string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                            Console.WriteLine("Received: " + message);
+
+                            BroadcastMessageToAll(message);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        BroadcastMessageToAll("disconnected:" + name);
+                        Console.WriteLine("Error: " + ex.Message);
+                        clientSockets.Remove(clientSockets.Where(s => s.Socket == clientSocket).SingleOrDefault());
+                        clientSocket.Close();
+                    }
+                }
+                else {
+                    sendMessage("Already exists"+name,clientSocket);
+                    clientSocket.Close();
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+               
+            }
+        }
+        private void sendMessage(string message, Socket tosend) {
+            try
+            {
+                tosend.Send(Encoding.ASCII.GetBytes(message));
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void BroadcastMessageExcept(string message, Socket excludeClient)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+
+            foreach (User clientSocket in clientSockets)
+            {
+                if (clientSocket.Socket != excludeClient)
+                {
+                    try
+                    {
+                        clientSocket.Socket.Send(buffer);
+                    }
+                    catch (Exception e) { }
+                }
+            }
+        }
+        private void BroadcastMessageToAll(string message)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+
+            foreach (User clientSocket in clientSockets)
+            {
+                try
+                {
+                    clientSocket.Socket.Send(buffer);
+                }
+                catch (Exception ex) { }
+                
+            }
+        }
+
+
+    }
+}
---
+++ PCappServer/classes/ClientHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCappServer.classes
+{
+    class ClientHandler
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private Form1 server;
+
+        public ClientHandler(TcpClient client, Form1 server)
+        {
+            this.client = client;
+            this.server = server;
+            stream = client.GetStream();
+        }
+
+        public void HandleClient()
+        {
+            try
+            {
+                byte[] data = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(data, 0, data.Length)) != 0)
+                {
+                    string message = Encoding.ASCII.GetString(data, 0, bytesRead);
+                    message = message.TrimEnd('\0'); // Remove null terminator
+                    Console.WriteLine("Received: " + message);
+
+                    server.BroadcastMessage(message, this);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Client disconnected: " + client.Client.RemoteEndPoint);
+                server.BroadcastMessage("Client disconnected: " + client.Client.RemoteEndPoint, this);
+            }
+            finally
+            {
+                server.RemoveClient(this);
+                client.Close();
+                stream.Close();
+            }
+        }
+        public void SendMessage(string message)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+        }
+
+        public void Stop()
+        {
+            client.Close();
+            stream.Close();
+        }
+    }
+}
